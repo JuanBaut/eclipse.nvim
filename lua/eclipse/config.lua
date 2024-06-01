f3d358d966bfcebcb307693276775338365b5157
JuanBaut <deserd@protonmail.com>
2024-06-01 00:43:13 -0500

    commented features related to old functionality
    (expand)

--- lua/eclipse/config.lua
+++ lua/eclipse/config.lua
@@ -8,21 +8,32 @@ local defaults = {
 		-- we try to get the foreground from the highlight groups or fallback color
 		color = { "Normal", "#ffffff" },
 		term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
+		--inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
 	},
+	--context = 10, -- amount of lines we will try to show arounc the current line
+	--treesitter = true, -- use treesitter when available for the filetype
+	---- treesitter is used to automatically expand the visible text,
+	---- but you can further control the types of nodes that should always be fully expanded
+	--expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+	--	"function",
+	--	"method",
+	--	"table",
+	--	"if_statement",
+	--},
 	exclude = {}, -- exclude these filetypes
 }
 
 --- @type EclipseOptions
 M.options = {}
 M.types = {}
-M.expand = {}
+--M.expand = {}
 
-function M.setup(options)
-	M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
-	for _, value in pairs(M.options.expand) do
-		M.expand[value] = true
-	end
-end
+--function M.setup(options)
+--	M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+--	for _, value in pairs(M.options.expand) do
+--		M.expand[value] = true
+--	end
+--end
 
 function M.colors()
 	local fg = "#ffffff"
